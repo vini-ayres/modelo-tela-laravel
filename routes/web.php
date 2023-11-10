6870775493b4437c73d9c0caac868703e2141fd6
Bianca <biancafdribeiro@gmail.com>
2023-11-09 22:16:54 -0300

    Front-end: tela de login (nao esta pronta)

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/login', function () {
     return view('login');
+});
+
+# Arrumar nome da rota
+Route::get('/login', function () {
+    return view('welcome');
 });
 Route::get('/form', function () {
     return view('form');
