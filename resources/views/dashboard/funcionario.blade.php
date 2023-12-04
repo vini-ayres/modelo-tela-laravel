8dd801df8cf03a0389c544622bb2c02ace22622e
vini-ayres <v.ayres@aluno.ifsp.edu.br>
2023-12-04 09:57:05 -0300

    Correção da dashboard funcionario

--- resources/views/dashboard/funcionario.blade.php
+++ resources/views/dashboard/funcionario.blade.php
@@ -3,7 +3,6 @@
 @section('links-sidebar')
     <a href="/dashboard-funcionario/form" onclick="changeTab(this)">Formulário de ordens de serviço</a>
     <a href="/dashboard-funcionario/minhas-solicitacoes" onclick="changeTab(this)">Minhas solicitações</a>
-    <a href="/dashboard-funcionario/tabela-solicitacoes" onclick="changeTab(this)">Minhas solicitações</a>
 @endsection('links-sidebar')
 
 @section('content')
